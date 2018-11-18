20676394b34df955cd6208bf6dbbb846a250ff49
Steve Winslow <swinslow@gmail.com>
2018-11-17 22:11:52 -0500

    Added HistoryTable react class
    Signed-off-by: Steve Winslow <swinslow@gmail.com>

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import ButtonExampleEmphasis from './ButtonExampleEmphasis';
+import HistoryTable from './HistoryTable';
 import logo from './logo.svg';
 import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      ready: true,
+      history: [
+        {"path": "/abc", "date": "2018-11-17T21:55:00Z"},
+        {"path": "/def", "date": "2018-11-16T15:22:00Z"},
+        {"path": "/ghi", "date": "2018-11-15T00:00:00Z"}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1>Hello!</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <HistoryTable ready={this.state.ready} history={this.state.history} />
           <a
             className="App-link"
             href="https://reactjs.org"
---
+++ webapp/src/HistoryTable.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Table } from 'semantic-ui-react'
+
+function createHistoryRows(history) {
+    var rows = [];
+    var i = 0;
+
+    history.forEach(function(h) {
+        const row = (
+            <Table.Row key={i}>
+                <Table.Cell>{h.path}</Table.Cell>
+                <Table.Cell>{h.date}</Table.Cell>
+            </Table.Row>
+        );
+        rows.push(row);
+        i = i + 1;
+    })
+
+    return rows;
+}
+
+function HistoryTable(props) {
+    const ready = props.ready;
+    if (!ready) {
+        return (
+            <div>Loading...</div>
+        );
+    }
+
+    const historyRows = createHistoryRows(props.history);
+    return (
+        <div>
+            <Table celled>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.HeaderCell>Path</Table.HeaderCell>
+                        <Table.HeaderCell>Date</Table.HeaderCell>
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    {historyRows}
+                </Table.Body>
+            </Table>
+        </div>
+    )
+}
+
+export default HistoryTable;
